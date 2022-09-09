6e8448c6c77c518ebd95990e419dddd8013db2db
KevinJablonski <113209802+KevinJablonski@users.noreply.github.com>
2022-09-09 17:29:29 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -21,7 +21,7 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = int(5)
-    elif question == "What layer of the TCP/IP model does the protocol ICPM belong to? - The answer should be an integer number":
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = int(3)
 
     else:
